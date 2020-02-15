118a7cd947eeb9443870041f63d108915e03ebb4
Bora Gulerman <fafadaw1234@gmail.com>
2020-02-15 00:50:58 -0800

    Initial Commit

---
+++ imageDetectionReality/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imageDetectionReality
+//
+//  Created by macos on 15.02.2020.
+//  Copyright © 2020 macos. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
